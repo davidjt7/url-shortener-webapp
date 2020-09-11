294a5a0ab4ce7cb103750dffe33f8cfb056b8aab
David Joseph Thomas <david.thomas@experionglobal.com>
2020-09-11 18:46:30 +0530

    API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import InputSection from './components/InputSection/InputSection';
+import ResultText from './components/ResultText/ResultText';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    shortUrl: '',
+    inputUrl: ''
+  });
   return (
-    <div>
+    <div className="App">
       <Header name="Url Shortener" />
-      <InputSection inputLabel="Enter link here" buttonLabel="Shorten url" />
+      <InputSection inputLabel="Enter link here" buttonLabel="Shorten url" callBack={displayResult(state, setState)} state={state} setState={setState} />
+      <ResultText result={state.shortUrl}></ResultText>
     </div>
 
   );
-}
+};
 
 export default App;
+
+const getUrl = async (inputUrl: string) => {
+  const result = await fetch('http://localhost:8001/links',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        link: inputUrl
+      })
+    });
+  return result.json();
+};
+
+const formatResponse = async (input: any) => {
+  const result = await getUrl(input);
+  return result.shortUrl;
+};
+
+const displayResult = (state: any, setState: any) => async () => setState({ ...state, shortUrl: await formatResponse(state.inputUrl) })
--- src/components/InputSection/InputSection.tsx
+++ src/components/InputSection/InputSection.tsx
@@ -5,10 +5,12 @@ import { Button, TextField } from '@material-ui/core';
 function InputSection(props: any) {
   return (
     <div className="Input-section">
-      <TextField className="Url-input" label={props.inputLabel} variant="outlined" />
-      <Button className="Shorten-button" variant="contained" color="primary">{props.buttonLabel}</Button>
+      <TextField className="Url-input" label={props.inputLabel} variant="outlined" onChange={captureInput(props)} />
+      <Button onClick={props.callBack} className="Shorten-button" variant="contained" color="primary">{props.buttonLabel}</Button>
     </div>
   );
 }
 
 export default InputSection;
+
+const captureInput = (props: any) => (e: any) => props.setState({ ...props.state, inputUrl: e.target.value });
